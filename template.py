ab5878591a6a5f4849e7c26bd156fb548dbea3a4
Laraib Ahmad Siddiqui <laraibks@gmail.com>
2025-05-24 20:59:51 +0530

    FS

--- template.py
+++ template.py
@@ -26,3 +26,13 @@ list_of_files = [
     "tox.ini",
     "experiments/experiment.ipynb"
 ]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass # 

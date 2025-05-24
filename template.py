65cb098028c1b990c2c2b28741f5f4aaee4c0b15
Laraib Ahmad Siddiqui <laraibks@gmail.com>
2025-05-24 20:45:48 +0530

    template

---
+++ template.py
@@ -0,0 +1,28 @@
+import os
+from pathlib import Path
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingession.py",
+    "src/components/data_transformation.py",
+    "src/components/model_evaluation.py",
+    "src/pipelines/__init__.py",
+    "src/pipelines/training_pipeline.py",
+    "src/pipelines/prediction_pipeline.py",
+    "src/utils/utils.py",
+    "src/utils/__init__.py",
+    "src/logger/logging.py",
+    "src/exception/exception"
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
+    "init_setup.sh",
+    "requirements.txt",
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "experiments/experiment.ipynb"
+]
